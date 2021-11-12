b8a666c9e243da665b9983c13d6edd1a30ccb7a2
Robel Campbell <29740744+x0rb3l@users.noreply.github.com>
2021-11-12 11:24:39 -0500

    updated

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,6 @@
+apt update && apt full-upgrade -y
+apt install golang terminator
+
 git clone https://github.com/x0rb3l/shell_profiles.git
 git clone https://github.com/x0rb3l/vim_one_monokai.git
 
@@ -8,3 +11,11 @@ mkdir ~/.vim
 cp -r ~/Downloads/vim_one_monokai/colors ~/.vim/
 
 echo "synax on\r\ncolorscheme one-monokai\r\nset termguicolors\r\nset number" > ~/.vimrc
+
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+
+mkdir ~/tools
+cd ~/tools
+git clone https://github.com/projectdiscovery/nuclei-templates.git
